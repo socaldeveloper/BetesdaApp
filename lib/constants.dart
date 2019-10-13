f21d4f6c084aaf43c6dcea85868443e59910562c
Walter <walterrcruz@yahoo.com>
2019-10-12 19:03:02 -0700

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const kTitleText = TextStyle(fontSize: 30, fontWeight: FontWeight.w700);
+const kSubTitleText = TextStyle(fontSize: 17, fontWeight: FontWeight.w500);
---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:betesda/constants.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(BetesdaApp());
+
+class BetesdaApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LandingPage(),
+    );
+  }
+}
+
+class LandingPage extends StatefulWidget {
+  @override
+  _LandingPageState createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Betesda'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            left: 12.0,
+                          ),
+                          child: Text(
+                            'Ministerios Betesda',
+                            style: kTitleText,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              left: 12.0, top: 8, bottom: 60),
+                          child: Text(
+                            'Casa de Misericordia',
+                            style: kSubTitleText,
+                          ),
+                        ),
+                      ],
+                    ),
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.lightBlueAccent,
+              child: Column(
+                children: <Widget>[],
+              ),
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    color: Colors.orange,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.blueGrey,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.yellow,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
